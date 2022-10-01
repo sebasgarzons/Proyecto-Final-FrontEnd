6404301ba3f61cda787f0588377f767ae606ba51
MaryF <maryanfer23@gmail.com>
2022-10-01 18:34:36 -0300

    fixed form js

--- js/newsletter.js
+++ js/newsletter.js
@@ -3,7 +3,6 @@
 const nombre = document.querySelector('#form__name');
 const email = document.querySelector('#form__email');
 const form = document.querySelector('#form');
-const required = document.querySelector('#form__email').required = false;
 
 
 // constates de los errores.
@@ -20,9 +19,11 @@ const button = document.getElementById('form__button');
 
 form.addEventListener('submit', (e)=>{    
     e.preventDefault();
+        
     validateName(nombre.value);
     validateEmail(email.value);
 
+    
 })
 
 
@@ -44,9 +45,12 @@ function validateEmail(email){
     let validation = /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/g;
 
     if(!validation.test(email)){
-        emailError.style.visibility = 'visible';    
+        emailError.style.visibility = 'visible';
+        
+         
         } else {
             emailError.style.visibility = 'hidden'
+            
         }
 
 
